6ae09ad65fb61a42fa2edb825411e2d5406a0be0
Koy <koykub333@hotmail.com>
2024-04-20 12:24:48 -0600

    updating moviedetail.js

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -29,20 +29,20 @@ class MovieDetail extends Component {
                     <ListGroup>
                         <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
                         <ListGroupItem>
-                            {this.props.selectedMovie.actors.map((actor, i) =>
+                            {this.props.selectedMovie.actors ? this.props.selectedMovie.actors.map((actor, i) =>
                                 <p key={i}>
                                     <b>{actor.actorName}</b> {actor.characterName}
-                                </p>)}
+                                </p>) : null }
                         </ListGroupItem>
                         <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
                     </ListGroup>
                     <Card.Body>
-                        {this.props.selectedMovie.reviews.map((review, i) =>
+                        {this.props.selectedMovie.reviews ? this.props.selectedMovie.reviews.map((review, i) =>
                             <p key={i}>
                                 <b>{review.username}</b>&nbsp; {review.review}
                                 &nbsp;  <BsStarFill /> {review.rating}
                             </p>
-                        )}
+                        ) : null }
                     </Card.Body>
                 </Card>
             )
